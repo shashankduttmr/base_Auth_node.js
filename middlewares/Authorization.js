8d742512274619bbdef6f5fffb6e4241f4b8b222
shashankduttmr <shashankduttm@gmail.com>
2023-01-06 15:23:51 +0530

    success commit

---
+++ middlewares/Authorization.js
@@ -0,0 +1,16 @@
+const User = require('../models/User')
+const Post = require('../models/Post')
+const Comment = require('../models/Comments')
+
+module.exports.PostAuthor = async function(req, res, next){
+    const {currentUser} = req.session
+    const {id} = req.params
+    const usr = await User.findById(currentUser)
+    const post = await Post.findById(id)
+    if(post.author.equals(usr._id)){
+        next()
+    }else{
+        req.flash('error', 'You are not authorised to do so')
+        res.redirect(`/post/${id}`)
+    }
+}
