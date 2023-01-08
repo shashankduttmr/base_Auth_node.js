d69dfba5c5ee92a9971d27bacba74be8674e8daa
shashankduttmr <shashankduttm@gmail.com>
2023-01-08 23:42:24 +0530

    first commit

--- validations/validations.js
+++ validations/validations.js
@@ -6,7 +6,7 @@ const PostSchema = joi.object({
     name:joi.string().required(),
     description:joi.string().required(),
     location:joi.string().required(),
-    deleteImaged:joi.array()
+    deleteImages:joi.array()
 }).required()
 
 module.exports.PostValidation = function(req, res, next){
