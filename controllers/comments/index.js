dfec711f4a8eb1d0931bb107c1d7ab57be8403b8
shashankduttmr <shashankduttm@gmail.com>
2023-01-10 21:41:38 +0530

    just to fix minor bugs

--- controllers/comments/index.js
+++ controllers/comments/index.js
@@ -4,6 +4,8 @@ const Post = require('../../models/Post')
 const User = require('../../models/User')
 
 module.exports.PostComment = async function(req, res, next){
-
+    const {id} = req.params
+    const {currentUser} = req.session
+    
 }
 
